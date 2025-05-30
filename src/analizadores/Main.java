6572ff3cb2ecd782a3037960a8e7c697a0a02fbe
vallit0 <evalleusac@gmail.com>
2025-05-30 02:33:41 -0600

    feat: no se que es esto pero va bien encaminado

--- src/analizadores/Main.java
+++ src/analizadores/Main.java
@@ -25,39 +25,12 @@ public class Main {
       } while (token.sym != sym.EOF);
 
       fr.close();
-      //Sintactico parser = new Sintactico(scanner);
-//
-//      // 2) Lanzamos el parseo; nuestras acciones de CUP llenarán Sintactico.salida
-//      parser.parse();
-//
-//      // 3) Obtenemos el HTML “crudo” generado
-//      String htmlSalida = Sintactico.salida.toString();
-//
-//      // 4) Escribimos el HTML sin cifrar
-//      try (FileWriter fw = new FileWriter("salida.html")) {
-//        fw.write(htmlSalida);
-//      }
-//
-//      // 5) Ciframos con A1Z26 y guardamos
-//      String htmlCifrado = Encriptador.cifrar(htmlSalida);
-//      try (FileWriter fw2 = new FileWriter("salida_cifrada.html")) {
-//        fw2.write(htmlCifrado);
-//      }
-//
-//      // 6) Desencriptamos el resultado cifrado y lo mostramos
-//      String htmlDesencriptado = Encriptador.desencriptar(htmlCifrado);
-//      System.out.println("=== Texto desencriptado ===");
-//      System.out.println(htmlDesencriptado);
-//
-//      System.out.println("¡Proceso completado! Revisa salida.html y salida_cifrada.html.");
-//
-//    } catch (IOException e) {
-//      System.err.println("Error de E/S: " + e.getMessage());
-//      e.printStackTrace();
-//    } catch (Exception e) {
-//      System.err.println("Error durante el parseo o cifrado: " + e.getMessage());
-//      e.printStackTrace();
-//    }
-      //}
+      // Aqui ya tokenizamos la entrada
+      // Ahora hay que reconocer el orden de la entrada y encriptarlo al mismo tiempo
+      System.out.println("Analizador Lexico Terminado");
+      // iniciamos el prceso de encriptado
+      Sintactico parser = new Sintactico(scanner);
+      //
+
     }
   }
